6bde5fd6bf12c502bd7143f7ec4723de66b399ed
AlejandroJSR7 <AlejandroJSR7@Gmail.com>
2018-12-02 21:19:22 -0500

    Header logo, path to home

--- app/shared/header/header.controller.js
+++ app/shared/header/header.controller.js
@@ -3,8 +3,14 @@ angular
   .module('elBaratonApp')
   .controller('HeaderController', headerController);
 
-  headerController.$inject = [];
-  function headerController() {
+  headerController.$inject = ['$location'];
+  function headerController($location) {
     const headerVM = this;
     headerVM.header_brand = 'El Baratón';
+    headerVM.goToPage = goToPage;
+
+    function goToPage(path_url) {
+      $location.url(path_url)
+    }
+    
   }
