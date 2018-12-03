cfa815805fccfb0d346c390780cbf574ff7e3a6c
AlejandroJSR7 <AlejandroJSR7@Gmail.com>
2018-12-03 06:03:51 -0500

    Improve Nav Menu

--- app/shared/categories/categories.controller.js
+++ app/shared/categories/categories.controller.js
@@ -3,14 +3,21 @@ angular
   .module('elBaratonApp')
   .controller('CategoriesController', categoriesController);
 
-  categoriesController.$inject = ['CategoriesService', '$routeParams'];
-  function categoriesController(categoriesService, $routeParams) {
+  categoriesController.$inject = ['CategoriesService', '$routeParams', '$location'];
+  function categoriesController(categoriesService, $routeParams, $location) {
     const categoriesVM = this;
     categoriesVM.title = 'Lista de categorías';
     categoriesVM.list_of_categories = [];
+    categoriesVM.goToUrl = goToUrl;
     
     categoriesService.getAllCategories().then(function(response) {
       categoriesVM.list_of_categories = response;
+      console.log('categoriesVM.list_of_categories', categoriesVM.list_of_categories)
     });
+
+    function goToUrl(sublevel) {
+      console.log('sublevel', sublevel)
+      $location.url('/category/products/' +  sublevel.id);
+    }
 
   }
